5e544f2bb07fbd9e27ccf213e69785c00e33bab5
Istabraq Kulow <istakulow@gmail.com>
2024-07-10 15:01:03 +0300

    filter

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = 0;
+
+const batteries = [
+  { assembled: 10 },
+  { assembled: 5 },
+  { assembled: 6 },
+  { assembled: 10 }
+];
+
+totalBatteries = batteries.reduce((acc, current) => acc + current.assembled, 0);
